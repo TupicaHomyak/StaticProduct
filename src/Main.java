0ead56f80cf2ec2ef2bf2286f8430d1aab053d92
Olga Malkova <Vodeviiil@mail.ru>
2023-03-02 19:23:05 +0300

    вв

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,27 @@ public class Main {
         System.out.println(Product.getAveragePrice());
         System.out.println(Product.getCount());
         System.out.println(Product.totalPrice);
+        System.out.println();
 
+
+        ArithmeticCalculator calculator = new ArithmeticCalculator(56, 12);
+        System.out.println("Сложение: " + calculator.calculate(Operation.ADD));
+        System.out.println("Вычитание: " + calculator.calculate(Operation.SUBTRACT));
+        System.out.println("Умножение: " + calculator.calculate(Operation.MULTIPLY));
+        System.out.println();
+
+        Engine engine = new Engine(
+                EngineType.DIESEL, 3.5, 156
+        );
+        Car car = new Car(engine, CarType.SUV, 2300);
+        System.out.println("Тип автомобиля: " + car.getType());
+        System.out.println("Масса двигателя: " + car.getWeight());
+        System.out.println("Тип двигателя: " +
+                car.getEngine().getType());
+        System.out.println("Объём двигателя: " +
+                car.getEngine().getVolume());
+        System.out.println("Мощность двигателя: " +
+                car.getEngine().getPower());
 
     }
 }
